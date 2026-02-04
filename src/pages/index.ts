f9c9b04743cb5e43a5cd52e379b83288ed6413a5
Vicenzo Scavino <vicenzoscavino52@gmail.com>
2026-02-03 22:07:50 -0500

    phase5: remove pages stubs

--- src/pages/index.ts
+++
@@ -1,8 +0,0 @@
-// Re-export all pages
-export { default as DiscoverPage } from './DiscoverPage';
-export { default as CategoryPage } from './CategoryPage';
-export { default as ProjectsPage } from './ProjectsPage';
-export { default as LibraryPage } from './LibraryPage';
-export { default as ProfilePage } from './ProfilePage';
-export { default as NotificationsPage } from './NotificationsPage';
-export { default as UserSearchPage } from './UserSearchPage';
