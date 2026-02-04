023ba67c1f9d2893720a62a3e5636972a9964444
Vicenzo Scavino <vicenzoscavino52@gmail.com>
2026-02-03 21:16:38 -0500

    phase5: remove posts stubs (block 2)

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,7 +1,6 @@
 // Re-export all pages
 export { default as DiscoverPage } from './DiscoverPage';
 export { default as CategoryPage } from './CategoryPage';
-export { default as FeedPage } from './FeedPage';
 export { default as ProjectsPage } from './ProjectsPage';
 export { default as LibraryPage } from './LibraryPage';
 export { default as ProfilePage } from './ProfilePage';
