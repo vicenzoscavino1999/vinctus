9073e1b3c3185de2f6b3effb4365eddf7b15c86a
vicenzoscavino1999 <vicenzoscavino52@gmail.com>
2026-02-03 22:45:25 -0500

    phase5: stop treating context exports as stubs (#56)

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,2 @@
 /* eslint-disable react-refresh/only-export-components */
-// TODO: STUB REMOVE BY: 2026-03-31
-// Ticket: PHASE2-017
 export { AuthProvider, useAuth, default } from '@/app/providers/AuthContext';
